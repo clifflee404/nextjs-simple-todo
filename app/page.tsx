f0a9c10aa4191e4a1644919bb661c4e2829ca580
cliff <clifflee1022@gmail.com>
2023-06-26 19:06:56 +0800

    rafactor: change title

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
   return (
     <main className="max-w-4xl mx-auto p-4">
       <div className="text-center my-5 flex flex-col gap-4">
-        <h1 className="text-2xl font-bold">Todo List App</h1>
+        <h1 className="text-2xl font-bold">Next.js Todo List App</h1>
       </div>
       <AddTask />
       <TodoList tasks={tasks} />
